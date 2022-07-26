867174614573461704a06b7b4263d9e1826e466c
amber liu <amber.sj.liu@gmail.com>
2022-07-25 23:58:55 -0400

    Initial Commit

---
+++ TestHome/ColorViewStruct.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewStruct.swift
+//  TestHome
+//
+//  Created by Swift Admin on 2022-07-25.
+//
+
+import Foundation
+import UIKit
+
+
+struct ColorView{
+    let view = UIView()
+    var locked = false
+    
+    
+}
